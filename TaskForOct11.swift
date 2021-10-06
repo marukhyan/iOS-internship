0016394230f2bad0e458df51a6859bb757be9602
marukhyan <59002642+marukhyan@users.noreply.github.com>
2021-10-06 10:48:55 +0400

    Updated

---
+++ TaskForOct11.swift
@@ -0,0 +1,38 @@
+/*
+ Task 1
+ Create an enum which conforms to Error protocol. Define 2 error cases in that enum, one error case name should be outOfBounds, for second set name which you want.
+ */
+
+enum arrayErrorList: Error {
+    case outOfBounds
+    case minusNumber
+}
+
+/*
+ Task 2
+ Write a function which can throw an error. Function should receive one index parameter and return Int value. Inside function create an array of Ints from 5 elements. Inside the function do checking and if index is out of range then throw outOfRange error defined in the enum. Otherwise return element for that index.
+ */
+
+func myFunction(index: Int) throws -> Int {
+    let arr = [2,3,-3,5,6]
+    if index < 0 || index >= arr.count {
+        throw arrayErrorList.outOfBounds
+    }
+    if arr[index] < 0 {
+        throw arrayErrorList.minusNumber
+    }
+    return arr[index]
+}
+
+/*
+ Task 3
+ Write a code with do/catch blocks and inside do block throw second error defined in your enum. Handle it in your catch block and print appropriate message.
+ */
+
+do {
+    try print(myFunction(index: 2))
+} catch arrayErrorList.outOfBounds {
+    print("Out of Bounds")
+} catch arrayErrorList.minusNumber {
+    print("Minus detected in array")
+}
