4c582532895da0f721a332d5cc27e9e04f30e846
marukhyan <59002642+marukhyan@users.noreply.github.com>
2021-11-03 00:15:35 +0400

    Updated

--- iOS_Homework_1/iOS_Homework_1/SceneDelegate.swift
+++ iOS_Homework_1/iOS_Homework_1/SceneDelegate.swift
@@ -27,8 +27,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print("App Become Active")
+        let mapView = window?.rootViewController as? ViewController
+        mapView?.label.text = ""
+        mapView?.switchButton.setOn(false, animated: false)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -42,9 +44,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        print("App Entered Background")
     }
 
 
